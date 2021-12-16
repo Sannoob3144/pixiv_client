826dca092410173b4fa521adb1df8fcc2e85d82b
Sannoob <cotwo0139@gmail.com>
2021-12-16 22:40:36 +0900

    feat(pixiv_api_constants.dart): add pixiv api endpoints

---
+++ lib/data/pixiv_api_constants.dart
@@ -0,0 +1,8 @@
+const PIXIV_APP_API= "app-api.pixiv.net"; // HTTPS
+const PIXIV_CALLBACK_PATH = "/web/v1/users/auth/pixiv/callback";
+const PIXIV_LOGIN_PATH = "/web/v1/login";
+
+const PIXIV_OAUTH_SECURE_API = "oauth.secure.pixiv.net"; // HTTPS
+const PIXIV_AUTH_TOKEN_URL = "/auth/token";
+
+const USER_AGENT = "PixivAndroidApp/5.0.234 (Android 11; Pixel 5)";
