595139c9c832bb24c1304a2b3aec95887f35c0b5
Sannoob <cotwo0139@gmail.com>
2021-12-16 22:42:08 +0900

    feat(main.dart): add main screen

---
+++ lib/screens/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:pixiv_client/screens/login.dart';
+
+class MainScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return LoginScreen();
+  }
+}
