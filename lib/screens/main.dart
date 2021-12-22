aea8bbc60ef92f608c1ee556e80d2acd41e77eb7
Sannoob <cotwo0139@gmail.com>
2021-12-22 21:24:51 +0900

    feat(lib\screens\main.dart): check account when app launch

--- lib/screens/main.dart
+++ lib/screens/main.dart
@@ -1,10 +1,54 @@
 import 'package:flutter/material.dart';
+import 'package:path/path.dart' as path;
 import 'package:sqflite/sqflite.dart';
-import 'package:pixiv_client/screens/login.dart';
 
-class MainScreen extends StatelessWidget {
+class MainScreen extends StatefulWidget {
+  @override
+  MainState createState() => MainState();
+}
+
+class MainState extends State<MainScreen> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
+
+  Future<bool> _loginDataExists() async {
+    final Database database = await openDatabase(
+      path.join(await getDatabasesPath(), 'data.db'),
+      version: 1,
+      onCreate: (db, version) {
+        print("Database on create.");
+        db.execute(
+            'create table loginData (accessToken text, refreshToken text);');
+      },
+    );
+    final List<Map<String, dynamic>> queryRes =
+        await database.query("loginData");
+    // return queryRes.length > 0 ? true : false;
+    return false;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return LoginScreen();
+    return Scaffold(
+        appBar: AppBar(title: Text("Main Screen")),
+        body: FutureBuilder(
+            future: _loginDataExists(),
+            builder: (BuildContext context, AsyncSnapshot<bool> snapShot) {
+              if (!snapShot.hasData) {
+                return CircularProgressIndicator();
+              } else if (snapShot.data == false) {
+                return Center(
+                    child: ElevatedButton(
+                        child: Text("Login with pixiv"),
+                        onPressed: () {
+                          Navigator.pushNamed(context, "/login");
+                        }));
+              } else {
+                return Text("Login data already in database");
+              }
+            }));
   }
 }
